596ff0f9cd7106e949ec16fdf5b24c8a3b0f8e7a
abhijetv <abhijetv@yahoo-inc.com>
2014-06-04 20:44:50 -0700

    Initial Commit

---
+++ RottenTomatoesDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoesDemo
+//
+//  Created by Abhijeet Vaidya on 6/4/14.
+//  Copyright (c) 2014 yahoo inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
